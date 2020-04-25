b0ccadfb86b75aca3b4a528b40f189b8f44851f3
Oscar Caballero <ocaballeror@tutanota.com>
2020-04-25 16:11:10 +0200

    Remove python 3.5 "support"

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
         'Environment :: Console',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
