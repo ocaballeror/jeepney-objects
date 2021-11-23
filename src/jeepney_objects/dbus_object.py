2f53a74ffc295b6c3d3fb1cb44a0ae36b888e3e2
Oscar Caballero <ocaballeror@tutanota.com>
2021-11-24 00:46:28 +0100

    Rewrite interfaces mapping as nodes

--- src/jeepney_objects/dbus_object.py
+++ src/jeepney_objects/dbus_object.py
@@ -1,10 +1,11 @@
 """
 Functions to test dbus-related functionality.
 """
+import inspect
 import logging
 from dataclasses import dataclass, field
 from multiprocessing import Process
-from typing import Tuple, Dict, Callable
+from typing import List, Tuple, Dict, Callable
 
 from jeepney.low_level import HeaderFields
 from jeepney.low_level import Message, MessageType
@@ -32,19 +33,33 @@ class DBusInterface:
     properties: Dict[Tuple[str, str], DBusProperty] = \
         field(default_factory=lambda: {})
 
+    def introspect(self):
+        msg = ""
+
+        for prop in self.properties:
+            msg += f'<property name="{prop.name}" type="{prop.signature}" acces="{prop.access}"/>\n'
+
+        for name, impl in self.methods.items():
+            msg += f'<method name="{name}">\n'
+            for arg in inspect.signature(impl).parameters:
+                msg += f'<arg name="{arg}" type="v" direction="in"/>\n'
+            msg += '<arg name="value" type="v" direction="out"/>\n'
+            msg += '<method/>'
+
+        return msg + '\n'
 
     def set_handler(self, method_name, handler):
         """
         Create a new method and set a handler for it.
         """
-        logging.debug('set_handler name=%s', method_name)
+        logging.warning('set_handler name=%s', method_name)
         self.methods[method_name] = handler
 
     def get_handler(self, method_name):
         """
         Retrieve the handler for a specific method.
         """
-        logging.debug('get_handler name=%s', method_name)
+        logging.warning('get_handler name=%s', method_name)
         if method_name in self.methods:
             return self.methods[method_name]
         raise KeyError(f"Unregistered method: '{method_name}'")
@@ -54,7 +69,7 @@ class DBusInterface:
         Set the value of an existing property, or create a new one if it
         doesn't already exist.
         """
-        logging.debug(
+        logging.warning(
             'set_property name=%s, signature=%s, value=%s',
             prop_name, signature, value
         )
@@ -72,7 +87,7 @@ class DBusInterface:
         """
         Get the value of a property. Raises a KeyError if it doesn't exist.
         """
-        logging.debug('get_property name=%s', prop_name)
+        logging.warning('get_property name=%s', prop_name)
         if prop_name not in self.properties:
             err = f"Property '{prop_name}' not registered on this interface"
             raise KeyError(err)
@@ -84,10 +99,66 @@ class DBusInterface:
         """
         Get all properties in this interface.
         """
-        logging.debug('get_all_properties')
+        logging.warning('get_all_properties')
         props = self.properties
         items = [(k, (v.signature, v.value)) for k, v in props.items()]
         return (items,)
+
+
+def introspectable_interface():
+    name = "org.freedestkop.DBus.Introspectable"
+    methods = {"Instrospect": DBusInterface.introspect}
+    properties = {}
+    return DBusInterface(name=name, methods=methods, properties=properties)
+
+
+@dataclass
+class Node:
+    name: str
+    interfaces: List[DBusInterface] = field(default_factory=lambda: [])
+    children: List["Node"] = field(default_factory=lambda: [])
+
+    def __post_init__(self):
+        self.interfaces.append(introspectable_interface())
+
+    def introspect(self):
+        header = """
+            <!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN" "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+        """
+
+        msg = f"{header}\n<node>"
+        for ifa in self.interfaces:
+            msg += ifa.introspect() + '\n'
+
+        for child in self.children:
+            msg += f'<node name="{child.name}"/>'
+
+        msg += "<node/>"
+        return msg
+
+
+@dataclass
+class PathAccessor:
+    root_node: Node
+
+    def __getitem__(self, item):
+        return self.get_node(item)
+
+    def get_node(self, path):
+        node = self.root_node
+
+        if path == '/':
+            return node
+
+        for part in path.lstrip("/").split("/"):
+            for child in node.children:
+                if child.name == part:
+                    node = child
+                    break
+            else:
+                raise KeyError(f"Path {path} not found?")
+
+        return node
 
 
 class DBusObject:
@@ -102,7 +173,7 @@ class DBusObject:
 
     def __init__(self):
         self.name = None
-        self.interfaces = {}
+        self.paths = PathAccessor(Node(''))
         self.listen_process = None
         self.conn = open_dbus_connection(bus='SESSION')
         # unwrap replies by default. this means that we get only get the
@@ -204,18 +275,18 @@ class DBusObject:
             self.listen_process.terminate()
 
     def set_property(self, path, interface, prop_name, signature, value):
-        key = (path, interface)
-        if key not in self.interfaces:
-            logging.debug('New interface at %s', key)
-            self.interfaces[key] = DBusInterface(interface)
-        self.interfaces[key].set_property(prop_name, signature, value)
+        node = self.paths[path]
+        if interface not in node.interfaces:
+            logging.warning('New interface at %s', interface)
+            node.interfaces[interface] = DBusInterface(interface)
+        node.interfaces[interface].set_property(prop_name, signature, value)
 
     def set_handler(self, path, interface, method_name, method):
-        key = (path, interface)
-        if key not in self.interfaces:
-            logging.debug('New interface at %s', key)
-            self.interfaces[key] = DBusInterface(interface)
-        self.interfaces[key].set_handler(method_name, method)
+        node = self.paths[path]
+        if interface not in node.interfaces:
+            logging.warning('New interface at %s', interface)
+            node.interfaces[interface] = DBusInterface(interface)
+        node.interfaces[interface].set_handler(method_name, method)
 
     def _handle_property_msg(self, msg):
         """
@@ -227,7 +298,7 @@ class DBusObject:
         method = hdr.fields[HeaderFields.member]
         body = list(msg.body)
         iface_name = body.pop(0)
-        iface = self.interfaces[(path, iface_name)]
+        iface = self[path].interfaces[iface_name]
         if method == 'Get':
             prop_name = body[0]
             signature, value = iface.get_property(prop_name)
@@ -246,20 +317,12 @@ class DBusObject:
         hdr = msg.header
         path = hdr.fields[HeaderFields.path]
         method_name = hdr.fields[HeaderFields.member]
-        iface = hdr.fields.get(HeaderFields.interface, None)
+        iface_name = hdr.fields.get(HeaderFields.interface, None)
 
-        if iface is not None:
-            method = self.interfaces[(path, iface)].get_handler(method_name)
-        else:
-            for (it_path, iface_name), iface in self.interfaces.items():
-                if it_path == path and method_name in iface.methods:
-                    method = iface.methods[method_name]
-                    break
-            else:
-                raise KeyError(f"Unregistered method: '{method_name}'")
+        iface = self[path].interfaces[iface_name]
+        method = iface.get_handler(method_name)
 
-        args = msg.body
-        signature, body = method(*args)
+        signature, body = method(*msg.body)
         return new_method_return(msg, signature, body)
 
     def handle_msg(self, msg):
@@ -270,7 +333,7 @@ class DBusObject:
         It invokes other methods to perform the necessary action and sends a
         response message if applicable.
         """
-        logging.debug('Received message %s', msg)
+        logging.warning('Received message %s', msg)
         hdr = msg.header
         if not hdr.message_type == MessageType.method_call:
             return
@@ -290,5 +353,5 @@ class DBusObject:
                 sender = msg.header.fields[HeaderFields.sender]
                 response.header.fields[HeaderFields.destination] = sender
                 response.header.fields[HeaderFields.sender] = self.name
-                logging.debug('Sending response %s', response)
+                logging.warning('Sending response %s', response)
                 self.conn.send_message(response)
